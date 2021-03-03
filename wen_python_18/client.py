ca22fa78e3aa2368c617b8f8d8c0124a3a1f12bb
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-03 20:04:39 +0200

    Add files via upload

--- wen_python_18/client.py
+++ wen_python_18/client.py
@@ -1,1 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import socket
+import time
+import threading
+
+def receving (name, sock, switch):
+	while not switch:
+		try:
+			while True:
+				# data - сообщение, addr - ip человека
+				data, addr = sock.recvfrom(1024) 
+				# декодирует сообщение и печатает в ВАШУ консоль
+				print('\n'+data.decode("utf-8"))
+				time.sleep(0.2)
+		except:
+			pass	
+
 
---
+++ wen_python_18/test_te.py
@@ -0,0 +1,6 @@
+try:
+	x = 10
+	s = x / 0
+	print(s)
+except:
+	print('На zero делить нельзя')
