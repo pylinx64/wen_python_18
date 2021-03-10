ae37002f71f6a8361f2a556d8ba07b8ed0081432
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-10 20:11:10 +0200

    Add files via upload

--- wen_python_18/client.py
+++ wen_python_18/client.py
@@ -4,6 +4,7 @@ import socket
 import time
 import threading
 
+
 def receving (name, sock, switch):
 	while not switch:
 		try:
@@ -16,4 +17,42 @@ def receving (name, sock, switch):
 		except:
 			pass	
 
+	
+# коробка для выключения, для подключение	
+shutdown = False
+join = False
 
+# ваш ip, порт
+host = socket.gethostbyname(socket.gethostname())
+port = 0
+
+# сервера ip, порт
+server = ('192.168.31.17', 11719)
+
+# сокет
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+s.setblocking(0)
+
+name = input('name > ')
+# отправляет сообщение
+s.sendto(("["+name+"] => join chat ").encode("utf-8"), server)
+time.sleep(0.2)
+
+# запускает сбор сообщений с сервера
+rT = threading.Thread(target = receving, args = ("RecvThread", s, shutdown))
+rT.start()
+
+# запускает отправку сообщений
+while shutdown == False:
+	try:
+		message = input("["+name+"] > ")
+		s.sendto(("["+name+"] > " + message).encode("utf-8"), server)
+		time.sleep(0.2)
+	except:
+		s.sendto(("["+name+"] <= left chat ").encode("utf-8"), server)
+		shutdown = True
+
+# прекращает сбор сообщений с сервера, закрывает сооединение
+rT.join()
+s.close()
