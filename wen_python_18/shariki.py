1d40ca4c5a0c5893b312806e761c76fb20839b5d
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-14 20:14:08 +0300

    Add files via upload

---
+++ wen_python_18/shariki.py
@@ -0,0 +1,31 @@
+import pyxel 
+import math
+import random
+
+# Глобальные переменные
+SCREEN = [256, 256]
+MAX_SPEED_BUBBLE = 1.8
+
+# создаем класс вектор
+class Vec2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+'''
+# vectorA - это экземпляр класса
+# Vec() - класс
+vectorA = Vec2(2, 3)
+print(vectorA.__dict__)    
+'''
+
+class Bubble:
+    def __init__(self):
+       pass 
+    
+    def update(self):
+        pass
+        
+
+
+
