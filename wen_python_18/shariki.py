199ede0e6d54466c46e29e2899f674d932ebd3c1
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-22 10:56:32 +0300

    Add files via upload

--- wen_python_18/shariki.py
+++ wen_python_18/shariki.py
@@ -20,12 +20,35 @@ print(vectorA.__dict__)
 '''
 
 class Bubble:
-    def __init__(self):
-       pass 
+    def __init__(self, x, y):
+        # радиус Bubble
+        self.r = random.uniform(3, 10)
     
     def update(self):
         pass
         
-
-
-
+        
+class App:
+    def __init__(self):
+        # создает экран
+        pyxel.init(SCREEN[0], SCREEN[1])
+        
+        # запускает мышку
+        pyxel.mouse(True)
+        
+        # запускает программу
+        pyxel.run(self.update, self.draw)     
+        
+    def update(self):
+        # проверяет нажата ли клавиша Q
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        
+    def draw(self):
+        # очищает экран
+        pyxel.cls(0)
+        
+        pyxel.text(SCREEN[0] / 2 - 30, SCREEN[1] / 2, "Hello Cyberpank 2088", pyxel.frame_count % 16)
+     
+# запускаем класс "Приложение"        
+App()
